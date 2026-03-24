85d92d3178b659e2493a8f938dd0fa22b56e8141
nathan_plltr <nathan.plltr@gmail.com>
2026-03-24 13:53:10 +0100

    Delete python directory

--- python/script6.py
+++
@@ -1,9 +0,0 @@
-a=-9
-print('a='+str(a))
-if a%2==0 :
-    print(f"{a} est un nombre pair")
-
-a=-10
-print('a='+str(a))
-if a%2==0:
-    print(f"{a} est un nombre pair")
--- python/script8.py
+++
@@ -1,6 +0,0 @@
-# Exemple test de 10 premiers chiffres
-for x in range(10):
-    if x%2==0 : 
-        print(f'x={x} est pair')
-    else : 
-        print(f'x={x} est impair')
